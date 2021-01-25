91126ff3eee788ddd25e544e30ee288ff90e1ad3
dmlwn31 <dmlwn31@gmail.com>
2021-01-25 15:12:15 +0900

    1/25 qna페이지 디자인 완료 및 ajax로드 완료

--- gold/pages/qna/qna.php
+++ gold/pages/qna/qna.php
@@ -46,71 +46,62 @@
           <div class="qnaBoxes deWeBoxes">
             <div class="qnaTable">
               <ul class="qnaList">
-                <li class="qnaTitle clear" >
+                <li class="qnaTitle clear">
                   <span class="qnaNum">번호</span>
                   <span class="qnaId">아이디</span>
                   <span class="qnaTit">제목</span>
                   <span class="qnaReg">등록일</span>
                   <span class="qnaHit">조회수</span>
                 </li>
-                <li class="qnaContent clear" >
-                  <span class="qnaNum">1</span>
-                  <span class="qnaId">dmlwn31</span>
-                  <span class="qnaTit"><a href="#">첫 게시글 입니다</a></span>
-                  <span class="qnaReg">2021-01-22</span>
-                  <span class="qnaHit">99</span>
-                </li>
-                <li class="qnaContent clear" >
-                  <span class="qnaNum">2</span>
-                  <span class="qnaId">nachall</span>
-                  <span class="qnaTit"><a href="#">두번쨰 게시글 입니다</a></span>
-                  <span class="qnaReg">2021-01-22</span>
-                  <span class="qnaHit">12</span>
-                </li>
-                <li class="qnaContent clear" >
-                  <span class="qnaNum">2</span>
-                  <span class="qnaId">nachall</span>
-                  <span class="qnaTit"><a href="#">두번쨰 게시글 입니다</a></span>
-                  <span class="qnaReg">2021-01-22</span>
-                  <span class="qnaHit">12</span>
-                </li>
+
+             
+
               </ul>
             </div>
             <!-- end of qna table -->
 
-          <div class="searchPaging clear">
-            <div class="search">
-              <form action="abc.pnp" naem="qnaSearch" class="clear">
-                <select name="searchSelect" id="" class="searchSelect">
-                  <option value="qnaWearchId">아이디</option>
-                  <option value="qnaWearchTitle">제목</option>
-                </select>
-                <input type="text" name="qnaSarchInput" placeholder="검색어를 입력해 주세요." class="qnaSarchInput">
-                <button type="button" class="qnaSearchBtn"><i class="fa fa-search"></i></button>
-              </form>
+            <div class="searchPaging clear">
+              <div class="search">
+                <form action="abc.php" name="qnaSearch" class="clear qnaSearch">
+                  <select name="searchSelect" id="" class="searchSelect">
+                    <option value="qnaSearchId">아이디</option>
+                    <option value="qnaSearchTitle">제목</option>
+                  </select>
+                  <input type="text" name="qnaSearchInput" placeholder="검색어를 입력해 주세요" class="qnaSearchInput">
+                  <button type="button" class="qnaSearchBtn"><i class="fa fa-search"></i></button>
+                </form>
+              </div>
+              <div class="paging">
+                <span class="firstPg"><i class="fa fa-angle-double-left"></i></span>
+                <span class="prevPg"><i class="fa fa-angle-left"></i></span>
+                <span class="PgNum active">1</span>
+                <span class="PgNum">2</span>
+                <span class="nextPg"><i class="fa fa-angle-right"></i></span>
+                <span class="lastPg"><i class="fa fa-angle-double-right"></i></span>
+              </div>
             </div>
-            <div class="paging">
-              <span class="firstPg"><i class="fa fa-angle-double-left"></i></span>
-              <span class="prevPg"><i class="fa fa-angle-left"></i></span>
-              <span class="pgNum active">1</i></span>
-              <span class="pgNum">2</i></span>
-              <span class="prevPg"><i class="fa fa-angle-right"></i></span>
-              <span class="lastPg"><i class="fa fa-angle-double-right"></i></span>
-            </div>
-          </div>
-          <!-- end of search paging -->
-          
-          <div class="writeBox">
-              <form action="abc.php" method="post">
-                <input type="text" placeholder="제목을 작성해 주세요.">
-                <input type="text" placeholder="질문을 작성해 주세요.">
-                <button class="writeBtn">글쓰기</button>
+            <!-- end of search paging -->
+
+            <div class="writeBox clear">
+              <div class="qnaGuide">
+                <span>글쓰기</span>
+                <span><?=$userid?></span>
+                <!-- <span><a href="#">로그인</a></span> -->
+              </div>
+              <form action="/gold/php_process/pages/qna_insert.php?id=<?=$userid?>" method="post" class="writeForm" name="writeForm">
+                <p class="qnaTitInput">
+                  <label for="qnaTitle">제목</label>
+                  <input type="text" name="qnaTitle" id="qnaTitle" placeholder="제목을 입력해 주세요.">
+                </p>
+                <p class="qnaTxtInput">
+                  <textarea name="qnaTxt" placeholder="내용을 입력해 주세요"></textarea>
+                </p>
               </form>
+              <button type="submit" class="qnaSubmit">등록</button>
             </div>
 
-          </div>
-          
-          
+          </div> 
+
         </div>
         <!-- end of center -->
   
@@ -123,8 +114,26 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/gold/js/custom.js"></script>
     <script src="/gold/js/web_design_page.js"></script>
+    <script src="/gold/js/qna_ajax.js"></script>
     <script>
-      
+      const qnaSubmit = document.querySelector(".qnaSubmit");
+      qnaSubmit.addEventListener('click', insertQan);
+
+      function insertQan(){
+        if(!document.writeForm.qnaTitle.value){
+          alert("제목을 입력해 주세요.");
+          document.writeForm.qnaTitle.focus();
+          return;
+        }
+
+        if(!document.writeForm.qnaTxt.value){
+          alert("내용을 입력해 주세요.");
+          document.writeForm.qnaTxt.focus();
+          return;
+        }
+
+        document.writeForm.submit();
+      }
     </script>
   </body>
 </html>
